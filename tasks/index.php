ad34d7099e3d689c797e83605fc030492a7d38e7
guzin-ka <D309123@edu.curio.nl>
2025-03-17 12:18:14 +0100

    first

--- tasks/index.php
+++ tasks/index.php
@@ -1,19 +1,25 @@
 <!doctype html>
 <html lang="en">
-
+<link rel="stylesheet" href="../css/main.css">
 <head>
     <title>Task List</title>
-    <?php require_once '../head.php'; ?>
+    <?php require_once 'head.php'; ?>
 </head>
-
+<header>
+    <nav>
+        <div class="taskList">
+            <a href="create.php" id="new-task" style="padding: 5px; width: 150px; text-decoration: none;">New Task</a>
+            <a href="done.php" id="completed-tasks" style="padding: 5px; text-decoration: none;">Completed Tasks</a>
+            <a href="notDone.php" id="not-done-tasks" style="padding: 5px; text-decoration: none;">Not Done Tasks</a>
+        </div>
+        <h1>Welkom bij DeveloperLand!</h1>
+        <img src="logo-big-v3.png" width="200" height="200">
+    </nav>
+</header>
 <body>
     
     <div class="container">
-        <h1>Task List</h1>
-        
-        <a href="create.php">New Task</a><br>
-        <a href="done.php">Completed Tasks</a>
-        <a href="notDone.php">Not Done Tasks</a> 
+
 
         <?php
         // Configuratie inladen
@@ -38,6 +44,5 @@
         }
         ?>
     </div>
-
 </body>
 </html>
